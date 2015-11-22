f9e97c8cf9f7adfb946e51b28f669732823aba74
kosamuel <samuel.ko@mail.utoronto.ca>
2015-11-21 23:24:34 -0500

    Updated stylesheets and app.js to handle profile page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var profile = require('./routes/profile');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(session({
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/profile', profile);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
