c3dde68da3386d8e2206eab3368857b181ce813c
kosamuel <samuel.ko@mail.utoronto.ca>
2015-11-26 23:53:24 -0500

    Added image upload feature to edit.

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var session = require('express-session');
-	
+var session = require('express-session');  
+var fs = require('fs');
+var path = require('path');
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/profilePics');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now()+'.png');
+  }
+})
+
+//var upload = multer({dest: './uploads'});
+var upload = multer({ storage: storage });
+
 router.get('/profile', function(req, res, next) {
 	res.render('profile', { comic: "super hero", 
 						    rented: "Yes", 
@@ -26,9 +41,9 @@ router.post('/updatepwd', function(req, res, next) {
 	console.log(req.body);
 });
 
-router.post('/upload', function(req, res, next) {
-	console.log(req.files);
-	//res.render('edit', {image: req.body.profilePic})
+router.post('/upload', upload.single('profilePic'), function(req, res) {
+	console.log(req.file);
+	res.redirect('/profile/edit');
 });
 
 module.exports = router;
