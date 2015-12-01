18890fd2ddeadd8afec44d97d618edeb81388441
kosamuel <samuel.ko@mail.utoronto.ca>
2015-12-01 18:51:12 -0500

    Added access to db.  Added Admin stuff.

--- routes/profile.js
+++ routes/profile.js
@@ -6,6 +6,7 @@ var path = require('path');
 var multer = require('multer');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var bcrypt = require('bcrypt-nodejs');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -18,11 +19,12 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
-/*
+
 mongoose.connect('mongodb://localhost:27017/', {
   user: '',
   pass: ''
 });
+
 
 // Check the status of this connection
 var db = mongoose.connection;
@@ -31,8 +33,9 @@ db.once('open', function callback () {
   console.log('Connected to MongoDB');
 });
 
-
-var UserSchema = mongoose.Schema({
+/*
+var Schema = mongoose.Schema;
+var Users = mongoose.model('Users', new Schema({
   level: Number,
   username: {
     type: String,
@@ -51,86 +54,119 @@ var UserSchema = mongoose.Schema({
   comic: String,
   rented: Boolean,
   rating: Number
-});
-
-
-var Users = mongoose.model('Users', UserSchema);
+}), 'users');
 */
+var Users = mongoose.model('Users');
 
 router.get('/', function(req, res, next) {
 
-  // Find the user and load their profile data
-  //Users.find({username: req.session.login}, function(err, user) {
-      /*
-      var comic;
-      if (user.comic == '') {
-        comic = 'None available';
-      } else {
-        comic = user.comic;
-      } 
-
-      var rented;
-      if (user.rented == '') {
-        rented = true
-      } else {
-        rented = user.rented
-      }
-
-      var image;
-      if (user.image == '') {
-        image = '/images/logo.png';
-      } else {
-        image = user.image;
-      }
-
-      var name;
-      if (user.displayName == '') {
-        name = 'user';
-      } else {
-        name = user.displayNmae;
-      }
-
-      var rating;
-      if (user.rating == '') {
-        rating = 0;
-      } else {
-        rating = user.rating;
-      }
-      
-      res.render('profile', {
-                comic: comic, 
-                rented: rented, 
-                image: image,
-                email: user.username, 
-                name: name, 
-                description: user.description,
-                authLevel: user.level,
-                rating: rating
-               });
-
-  });
-  */
   if (typeof req.session.login !== 'undefined'){
-    res.render('profile', { comic: "super hero", 
-                rented: true, 
-                image: '/profilePics/logo.png', 
-                user: 'user here', 
-                email: 'email', 
-                name: 'name', 
-                description: 'description',
-                authLevel: 0,
-                rating: 4
+
+    // Find the user and load their profile data
+    Users.find({username: req.session.login}, function(err, currentUser) {
+
+      console.log(req.session.profileOwner);
+      var queryUser;
+      if (req.session.profileOwner === undefined || req.session.profileOwner != '') {
+        queryUser = req.session.login;
+      } else {
+        /***********Change this query for different profiles (other than your own)******************/
+        queryUser = req.session.profileOwner;
+      }
+      console.log(queryUser);
+
+      Users.find({username: queryUser}, function(err, user) {
+
+        var comic;
+        if (user[0].comic === undefined || user[0].comic == '') {
+          comic = 'None available';
+        } else {
+          comic = user[0].comic;
+        } 
+
+        var rented;
+        if (user[0].comic === undefined || user[0].rented == '' ) {
+          rented = true
+        } else {
+          rented = user[0].rented
+        }
+
+        var image;
+        if (user[0].comic === undefined || user[0].image == '' ) {
+          image = '/images/logo.png';
+        } else {
+          image = user[0].image;
+        }
+
+        var name;
+        if (user[0].comic === undefined || user[0].displayName == '') {
+          name = 'user';
+        } else {
+          name = user[0].displayNmae;
+        }
+
+        var rating;
+        if (user[0].comic === undefined || user[0].rating == '') {
+          rating = 0;
+        } else {
+          rating = user[0].rating;
+        }
+
+        // Get the current user's authority level
+        var access;
+        // Access for super admin
+        if (currentUser[0].level == 0 && req.session.login != user[0].username) {
+          access = 0;
+
+        // Access to the user's own account
+        } else if (req.session.login == user[0].username) {
+          access = 3;
+
+        // Lowest level access if the authority levels are the same
+        } else if (currentUser[0].level == user[0].level && req.session.login != user[0].username) {
+          access = 2;
+
+        // Access for admins 
+        } else if (currentUser[0].level == 1 && user[0].level == 2) {
+          access = 1;
+
+        }
+
+        var description;
+        if (description === undefined) {
+          description = '';
+        }
+      
+        req.session.profileOwner = '';
+
+        res.render('profile', {
+                  comic: comic, 
+                  rented: rented, 
+                  image: image,
+                  email: user[0].username, 
+                  name: name, 
+                  description: description,
+                  //authLevel: access,
+                  authLevel: 0,
+                  rating: rating,
+                  profileLevel: user[0].level
                });
+
+      });
+  
+   });  
+  
   } else {
     res.redirect('/');
+
   }
 
 });
 
-router.get('/edit', function(req, res, next) {
+router.post('/edit', function(req, res, next) {
 
   if (typeof req.session.login !== 'undefined'){
-    res.render('edit');
+    res.render('edit', {email: req.body.email, passnotempty: true, wrongpassword: false, newpasswordnotempty: true, matching: true});
   } else {
     res.redirect('/');
   }
@@ -138,28 +174,23 @@ router.get('/edit', function(req, res, next) {
 });
 
 router.post('/updateprofile', function(req, res, next) {
-	console.log(req.body);
-  if (typeof req.session.login !== 'undefined'){
 
-    Users.find({username: req.session.login}, function(err, user) {
+  if (typeof req.session.login !== 'undefined') {
 
-      // Change the username/email
-      if (req.body.email != '') {
-        user.username = req.body.email;
-      }
+    Users.find({username: req.body.email}, function(err, user) {
 
       // Change the display name
       if (req.body.displayName != '') {
-        user.displayName = req.displayName;
+        user[0].displayName = req.body.displayName;
       }
 
       // Change the description
       if (req.body.description != '') {
-        user.description = req.body.description;
+        user[0].description = req.body.description;
       }
 
       // Save it to the DB.
-      user.save(function(err) {
+      user[0].save(function(err) {
         if (err) {
           res.status(500).send(err);
           console.log(err);
@@ -169,42 +200,51 @@ router.post('/updateprofile', function(req, res, next) {
 
     });
 
-    res.render('edit');
+    res.render('edit', {email: req.body.email, passnotempty: true, wrongpassword: false, newpasswordnotempty: true, matching: true});
+
   } else {
     res.redirect('/');
   }
 });
 
 router.post('/updatepwd', function(req, res, next) {
-	console.log(req.body);
+
+  console.log(req.body);
+
   if (typeof req.session.login !== 'undefined'){
     
-    if (!bcrypt.compareSync(req.body.password, user.password)) {
-      res.render('edit', { title: 'CURD App', passnotempty: true, wrongpassword: true, newpasswordnotempty: true, matching: true });
+    Users.find({username: req.body.email}, function(err, user) {
+
+      var passEncrypted1 = bcrypt.hashSync(req.body.newPassword);
+
+      if (!bcrypt.compareSync(req.body.password, user[0].password)) {
+        res.render('edit', { title: 'CURD App', email: req.body.email, passnotempty: true, wrongpassword: true, newpasswordnotempty: true, matching: true });
     
-    } else if (req.body.password == '') {
-      res.render('edit', { title: 'CURD App', passnotempty: false, wrongpassword: false, newpasswordnotempty: true, matching: true });
+      } else if (req.body.password == '') {
+        res.render('edit', { title: 'CURD App', email: req.body.email, passnotempty: false, wrongpassword: false, newpasswordnotempty: true, matching: true });
 
-    } else if (req.body.newPassword == '') {
-      res.render('edit', { title: 'CURD App', passnotempty: true, wrongpassword: false, newpasswordnotempty: false, matching: true });
+      } else if (req.body.newPassword == '') {
+        res.render('edit', { title: 'CURD App', email: req.body.email, passnotempty: true, wrongpassword: false, newpasswordnotempty: false, matching: true });
 
-    } else if (!bcrypt.compareSync(req.body.newPassword, req.body.confirmPassword)) {
-      res.render('edit', { title: 'CURD App', passnotempty: true, wrongpassword: false, newpasswordnotempty: true, matching: false });
+      } else if (req.body.newPassword != req.body.confirmPassword) {
+        res.render('edit', { title: 'CURD App', email: req.body.email, passnotempty: true, wrongpassword: false, newpasswordnotempty: true, matching: false });
     
-    } else {
-      user.password = req.body.newPassword;
+      } else {
 
-      user.save(function(err) {
-        if (err) {
-          res.status(500).send(err);
-          console.log(err);
-          return;
-        }
-      });
+        user[0].password = passEncrypted1;
 
-      res.render('edit', { title: 'CURD App', passnotempty: true, wrongpassword: false, newpasswordnotempty: true, matching: true });
+        user[0].save(function(err) {
+          if (err) {
+            res.status(500).send(err);
+            console.log(err);
+            return;
+          }
+        });
 
-    }
+        res.render('edit', { title: 'CURD App', email: req.body.email, passnotempty: true, wrongpassword: false, newpasswordnotempty: true, matching: true });
+
+      }
+  });
 
   } else {
     res.redirect('/');
@@ -214,19 +254,56 @@ router.post('/updatepwd', function(req, res, next) {
 router.post('/upload', upload.single('profilePic'), function(req, res) {
 
   if (typeof req.session.login !== 'undefined'){
-    Users.find({username: req.session.login}, function(err, user) {    
-      if (user.image != '/profilePics/logo.png') {
-        fs.unlink(user.image);
+    Users.find({username: req.body.email}, function(err, user) {    
+      if (user[0].image != '/profilePics/logo.png') {
+        fs.unlink(user[0].image);
       }
       
-      user.image = '/profilePics/' + req.file.filename;
+      user[0].image = '/profilePics/' + req.file.filename;
 
-      res.redirect('/profile/edit');
+      res.render('edit', { title: 'CURD App', email: req.body.email, passnotempty: true, wrongpassword: false, newpasswordnotempty: true, matching: true });
+
     });
   } else {
     res.redirect('/');
 
   }
+});
+
+router.post('/deleteAccount', function(req, res) {
+
+  Users.remove({username: req.body.email}, function(err) {
+    if (err) {
+      res.status(500).send(err);
+      console.log(err);
+      return;
+    }
+  });
+  res.redirect('/');
+
+});
+
+router.post('/makeAdmin', function(req, res) {
+
+  Users.find({username: req.body.email}, function(err, user) {
+    if (user[0].level == 1) {
+      user[0].level = 2;
+    } else if (user[0].level == 2) {
+      user[0].level = 1;
+    }
+
+    user[0].save(function(err) {
+      if (err) {
+        res.status(500).send(err);
+        console.log(err);
+        return;
+      }
+    });
+
+    req.session.profileOwner = req.body.email;
+    res.redirect('/profile');
+
+  });
 });
 
 module.exports = router;
