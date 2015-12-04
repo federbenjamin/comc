f885efeb24bb4df73cb3d2b075db439e31a0d29b
kosamuel <samuel.ko@mail.utoronto.ca>
2015-12-03 22:29:39 -0500

    Added delete review post action.

--- routes/readuserreview.js
+++ routes/readuserreview.js
@@ -4,15 +4,56 @@ var session = require('express-session');
 var mongoose = require('mongoose');
 
 var Reviews = mongoose.model('Reviews');
+var Users = mongoose.model('Users');
 
 router.post('/', function(req, res) {
 
-	Reviews.find({ username: req.body.email }, function(err, reviews) {
+	if (req.session.login !== undefined) {
 
-		console.log(reviews);
-		res.render('readuserreview', { title: "COMC", review: reviews, user: req.body.email });
+		var userQuery;
+		if (req.session.reviewOwner === undefined || req.session.reviewOwner == '') {
+			userQuery = req.body.email;
 
-	});
+		} else {
+			userQuery = req.session.reviewOwner;
+
+		}
+
+		req.session.reviewOwner = '';
+
+		var authLevel;
+		Users.find({ username: req.session.login }, function(err, user) {
+				authLevel = user[0].level;
+
+		});
+
+		Reviews.find({ username: userQuery }, function(err, reviews) {
+			res.render('readuserreview', { title: "COMC", review: reviews, user: userQuery, authLevel: authLevel, currentUser: req.session.login});
+
+		});
+	} else {
+		res.redirect('/');
+
+	} 
+
+});
+
+router.post('/deleteReview', function(req, res) { 
+  Reviews.remove({username: req.body.user, 
+  				  poster: req.body.poster, 
+  				  date: req.body.timestamp,
+  				  comment: req.body.comment,
+  				  rating: req.body.rating}, 
+  				  function(err) {
+    if (err) {
+      res.status(500).send(err);
+      console.log(err);
+      return;
+    }
+  });
+	
+  req.session.profileOwner = req.body.user.username;
+  res.redirect('/');
 
 });
 
